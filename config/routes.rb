81fa5a72f71e186e9d9a598d0a802ad63d281e98
Lutfiana Sari Ariestien <lutefnyew@gmail.com>
2018-08-30 06:34:19 +0900

    add admin home

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :restaurants
     resources :masjids
     resources :images
+    get '/', to: 'home#index'
   end
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
